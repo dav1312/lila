f9d7150873f791373d294116a26c825267b58dd7
dav1312 <63931154+dav1312@users.noreply.github.com>
2026-01-24 01:09:07 +0100

    i18n

--- ui/lobby/src/view/pools.ts
+++ ui/lobby/src/view/pools.ts
@@ -74,7 +74,7 @@ export function render(ctrl: LobbyController) {
         icon = undefined;
       }
 
-      const subLabel = custom ? (custom.mode === 'rated' ? 'Rated' : 'Casual') : pool.perf;
+      const subLabel = custom ? (custom.mode === 'rated' ? i18n.site.rated : i18n.site.casual) : pool.perf;
 
       return h(
         'div.lpool',
